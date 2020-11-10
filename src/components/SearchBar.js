a66a80a1c6541eccd518a5e81f9d6e9ec8a97363
heathkev <kevintuazon45@gmail.com>
2020-11-11 00:01:19 +0800

    added search screen and search bar installed axios create api service yelp

---
+++ src/components/SearchBar.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { Feather } from '@expo/vector-icons';
+import { TextInput } from 'react-native-gesture-handler';
+
+const SearchBar = ({ term, onTermChange, onTermSubmit }) => {
+    return (
+        <View style={styles.backgroundStyle}>
+            <Feather name="search" style={styles.iconStyle} />
+            <TextInput
+                style={styles.inputSyle}
+                placeholder="Search..."
+                value={term}
+                autoCapitalize="none"
+                autoCorrect={false}
+                onChangeText={onTermChange}
+                onEndEditing={onTermSubmit}
+                />
+        </View>
+    )
+};
+
+const styles = StyleSheet.create({
+    backgroundStyle: {
+        marginTop: 10,
+        backgroundColor: '#F0EEEE',
+        height: 50,
+        borderRadius: 5,
+        marginHorizontal: 15,
+        flexDirection: "row",
+    },
+
+    iconStyle: {
+        fontSize: 35,
+        color: 'black',
+        alignSelf: "center",
+        marginHorizontal: 15
+    },
+
+    inputSyle: {
+        flex: 1,
+        fontSize: 18
+    }
+});
+
+export default SearchBar;
