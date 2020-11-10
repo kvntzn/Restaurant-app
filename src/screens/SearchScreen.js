160a6110515765be45ba53754d7f57590de6f0d4
heathkev <kevintuazon45@gmail.com>
2020-11-11 01:32:53 +0800

    implemented results list component

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,11 +1,19 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import SearchBar from '../components/SearchBar';
-import useResults from '../hooks/useResults'
+import useResults from '../hooks/useResults';
+import ResultsList from '../components/ResutsList';
 
 const SearchScreen = () => {
     const [term, setTerm] = useState('');
     const [searchApi, results, errorMessage] = useResults();
+
+    const filterResultsByPrice = (price) => {
+        // price === '$' || '$$' || '$$$'
+        return results.filter(result => {
+            return result.price === price;
+        });
+    };
 
     return (
         <View>
@@ -15,10 +23,12 @@ const SearchScreen = () => {
                 onTermSubmit={() => searchApi(term)} />
             {errorMessage ? <Text>{errorMessage}</Text> : null}
             <Text>We have found {results.length} results.</Text>
+            <ResultsList results={filterResultsByPrice('$')} title="Cost Effective" />
+            <ResultsList results={filterResultsByPrice('$$')} title="Bit Pricier" />
+            <ResultsList results={filterResultsByPrice('$$$')} title="Big Spender" />
         </View>
     );
 };
 
-const styles = StyleSheet.create({});
 
 export default SearchScreen;
