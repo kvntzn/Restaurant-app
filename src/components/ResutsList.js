a826e25423a37259f4da3fd96effd5b9195179c8
heathkev <kevintuazon45@gmail.com>
2020-11-11 12:28:02 +0800

    Added styling and image display

--- src/components/ResutsList.js
+++ src/components/ResutsList.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import { View, Text, StyleSheet, FlatList } from 'react-native';
+import ResultsDetails from './ResultsDetail';
+import ResultsDetail from './ResultsDetail';
 
 const ResultsList = ({ title, results }) => {
     return (
-        <View>
+        <View style={styles.container}>
             <Text style={styles.titleStyle}>{title}</Text>
             <FlatList
                 horizontal
+                showsHorizontalScrollIndicator={false}
                 data={results}
                 keyExtractor={(result) => result.id}
                 renderItem={({ item }) => {
-                    return <Text>{item.name}</Text>
+                    return <ResultsDetails result={item} />
                 }}
             />
         </View>
@@ -20,7 +23,12 @@ const ResultsList = ({ title, results }) => {
 const styles = StyleSheet.create({
     titleStyle: {
         fontSize: 18,
-        fontWeight: 'bold'
+        fontWeight: 'bold',
+        marginLeft: 15,
+        marginBottom: 5
+    },
+    container: {
+        marginBottom: 10
     }
 });
 
